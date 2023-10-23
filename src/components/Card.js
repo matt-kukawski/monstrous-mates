4fcf6f0f67b4975007f4aaa82b532489a65aeab7
Matt Kukawski <matt@sonoflilith.com>
2023-10-23 20:04:24 +0100

    Update Card.js
    Using robohash instead of adorable.io (deprecated)

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default ({ name, email, id }) => {
   return (
     <div className='tc bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='card-img' src={`https://api.adorable.io/avatars/200/${id}.png`} />
+      <img alt='card-img' src={`https://robohash.org/${id}?size=200x200&set=set2`} /> 
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
